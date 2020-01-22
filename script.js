9ea3f2bf03a574a5c57b4419a77b6e216e4e5943
Tim Gentry <gentry.tim@gmail.com>
2020-01-21 22:13:28 -0500

    Added choice prompt

--- script.js
+++ script.js
@@ -1,27 +1,34 @@
-function computerPlay() {
+function game() {
   const playOptions = ["rock", "paper", "scissors"];
-  const randomPlay =
-    playOptions[Math.floor(Math.random() * playOptions.length)];
-  return randomPlay;
-}
-
-function playRound(computerSelection, playerSelection) {
-  //Convert all inputs to uniform lowercase
+  const playerSelection = prompt("Rock, Paper, or Scissors?");
+  const computerSelection = computerPlay();
   const compMove = computerSelection.toLowerCase();
   const playerMove = playerSelection.toLowerCase();
 
-  //Determine round winner
-  if (compMove === "rock" && playerMove === "scissors") {
-    return "You lose! Rock beats scissors.";
-  } else if (compMove === "paper" && playerMove === "rock") {
-    return "You lose! Paper beats rock.";
-  } else if (compMove === "scissors" && playerMove === "paper") {
-    return "You lose! Scissors beats paper.";
-  } else {
-    return "You are the winner!";
-  }
-}
 
-const playerSelection = "PaPer";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+  function computerPlay() {
+    const randomPlay = playOptions[Math.floor(Math.random() * playOptions.length)];
+    console.log(randomPlay);
+    return randomPlay;
+  };
+
+  function playRound() {
+    console.log(playerMove);
+    if (compMove === "rock" && playerMove === "scissors") {
+      return "You lose! Rock beats scissors.";
+    } else if (compMove === "paper" && playerMove === "rock") {
+      return "You lose! Paper beats rock.";
+    } else if (compMove === "scissors" && playerMove === "paper") {
+      return "You lose! Scissors beats paper."
+    } else if (compMove === playerMove) {
+      return "It's a tie!"
+    } else if (playerMove === ("rock", "paper", "scissors")) {
+      return "You are the winner!";
+    } else {
+      return "You did something wrong."
+    }
+  };
+  console.log(playRound());
+};
+
+game();
